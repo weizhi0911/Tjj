7ec69ffac265a59aecc7618e4e240ecd4af215a1
wwf <wwf1466228266@outlook.outlook.com>
2021-09-03 23:44:10 +0800

    【修改】名字修改

--- renxi/love.js
+++ renxi/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana";
-	    ctx.fillText("  唐晶晶", 23, 10);
+	    ctx.fillText("  唐宝宝", 23, 10);
             ctx.restore();
         },
         clear: function() {
